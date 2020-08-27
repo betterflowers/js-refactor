c7bf53e3461a4ac15a23a27f7a54c10ffe5d1501
CHENKA10 <1054952496@qq.com>
2020-08-27 16:30:08 +0800

    [karol] refactor:extract countAmountByPlayType function

--- src/statement.js
+++ src/statement.js
@@ -16,6 +16,28 @@ function countCredits(perf, play) {
   return thisCredits;
 }
 
+function countAmountByPlayType(play, perf) {
+  let thisAmount = 0;
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+  }
+  return thisAmount;
+}
+
 function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -23,26 +45,8 @@ function statement (invoice, plays) {
 
   const format = formatUSD();
   for (let perf of invoice.performances) {
-    let thisAmount = 0;
     const play = plays[perf.playID];
-    // let thisAmount = 0;
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = countAmountByPlayType(play, perf);
     let thisCredits = countCredits(perf, play);
     volumeCredits += thisCredits;
     //print line for this order
