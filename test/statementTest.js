3f0a9ecebaa444faa23aaac08d6540445b87a83f
CHENKA10 <1054952496@qq.com>
2020-08-27 19:26:20 +0800

    [karol] test:add htmlFormat function test

--- test/statementTest.js
+++ test/statementTest.js
@@ -1,5 +1,5 @@
 const test = require('ava');
-const {statement} = require('../src/statement');
+const {statement,htmlFormat} = require('../src/statement');
 
 const plays = {
   'hamlet': {
@@ -160,4 +160,49 @@ test('statement case 7. Customer BigCo has one unknown performance. ', t => {
   catch (e) {
     t.is(e.message, 'unknown type: tragedy1');
   }
+});
+
+test('statement case 8', t => {
+  const invoice = {
+    'customer': 'BigCo',
+    'performances': [
+      {
+        'playID': 'hamlet',
+        'audience': 55,
+      },
+      {
+        'playID': 'as-like',
+        'audience': 35,
+      },
+      {
+        'playID': 'othello',
+        'audience': 40,
+      },
+    ],
+  };
+  const plays = {
+    'hamlet': {
+      'name': 'Hamlet',
+      'type': 'tragedy',
+    },
+    'as-like': {
+      'name': 'As You Like It',
+      'type': 'comedy',
+    },
+    'othello': {
+      'name': 'Othello',
+      'type': 'tragedy',
+    },
+  };
+  //when
+  const result = htmlFormat(invoice, plays);
+  t.is(result, '<h1>Statement for BigCo</h1>\n' +
+      '<table>\n' +
+      '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
+      ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
+      ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
+      ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
+      '</table>\n' +
+      '<p>Amount owed is <em>$1,730.00</em></p>\n' +
+      '<p>You earned <em>47</em> credits</p>\n');
 });
